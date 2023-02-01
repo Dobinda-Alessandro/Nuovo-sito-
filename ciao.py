1c49c7a245f92c1235a3f077b827ce7838726436
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2023-02-01 22:44:52 +0000

    2

---
+++ ciao.py
@@ -0,0 +1,19 @@
+  .box-move  {
+            position:absolute;
+            float: left;
+           margin-left: 700px;
+           top: -20px;
+
+.container  {
+    position: relative;
+    text-align: center;
+    color: black;
+}
+
+center {
+
+    position: absolute;
+    top: 1100px;
+  
+    
+   }
